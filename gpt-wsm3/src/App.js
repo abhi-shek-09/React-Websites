8010c01e159af0a46213d212c75e69bff7e2b900
unknown <abhi0902@hotmail.com>
2023-10-16 15:59:54 +0530

    navbar-created

--- gpt-wsm3/src/App.js
+++ gpt-wsm3/src/App.js
@@ -1,22 +1,24 @@
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, Whatgpt, Header } from './Containers';
+import { CTA, Brand, Navbar } from './Components';
+
 import './App.css';
-import {Blog, Features, Footer, Header, Possibility, Whatgpt} from './Containers'
-import {Brand, CTA, Navbar} from './Components'
-function App() {
-  return (
-    <div className="App">
-      <div className='gradient__bg'>
-        <Navbar/>
-        <Header/>
-      </div>
-      <Brand/>
-      <Whatgpt/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <Whatgpt />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
