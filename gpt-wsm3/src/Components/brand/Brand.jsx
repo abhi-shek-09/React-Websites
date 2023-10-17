db9c5e4c29f584ac0a37b5b82c279c8fb18650b1
unknown <abhi0902@hotmail.com>
2023-10-17 14:33:30 +0530

    header done

--- gpt-wsm3/src/Components/brand/Brand.jsx
+++ gpt-wsm3/src/Components/brand/Brand.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './Brand.css'
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './Brand.css';
 
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="dummy-text"/>
+    </div>
+    <div>
+      <img src={slack} alt="dummy-text"/>
+    </div>
+    <div>
+      <img src={atlassian} alt="dummy-text"/>
+    </div>
+    <div>
+      <img src={dropbox} alt="dummy-text"/>
+    </div>
+    <div>
+      <img src={shopify} alt="dummy-text"/>
+    </div>
+  </div>
+);
 
-export default Brand
+export default Brand;
