5fb9e628d2ca2d72905e74637bdaad09ed218e74
unknown <abhi0902@hotmail.com>
2023-10-18 10:16:38 +0530

    introd feature

--- gpt-wsm3/src/Components/feature/Feature.jsx
+++ gpt-wsm3/src/Components/feature/Feature.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-'>
+        <div>
+        <h1> {title}</h1>
+        </div>
+        <div className='gpt3__features-container_feature-text'>
+          {text}
+        </div>
+      </div>
+      </div>
   )
 }
 
