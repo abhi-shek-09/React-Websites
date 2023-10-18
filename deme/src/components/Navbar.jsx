ccb9467659e811a6c9586941dee0eee8c43da763
unknown <abhisheklfps@gmail.com>
2023-10-18 17:56:23 +0530

    redid everything-nav start

--- deme/src/components/Navbar.jsx
+++ deme/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+// Navbar.js
 import React, { useState } from 'react';
 import './navbar.css';
 import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
@@ -16,7 +17,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className='navbar__heading'>
-          <h3 className='navbar__heading-children'>MIRA MIKATI</h3>
+          <h1>MIRA MIKATI</h1>
         </div>
         <div className='navbar__links-container2'>
           <p><a href='#About' alt='About Us'>About Us</a></p>
@@ -26,17 +27,17 @@ const Navbar = () => {
             ? <RiCloseLine color="#000" size={30} />
             : <RiMenu3Line color="#000" size={30} />}
         </div>
-        {toggleMenu && (
-          <div className="navbar__menu-container scale-up-center">
-            <div className="navbar__menu-links">
-              <p><a href='#Shop' alt='Shop'>Shop</a></p>
-              <p><a href='#Collections' alt='Collections'>Collections</a></p>
-              <p><a href='#Ourworld' alt='Our world'>Our world</a></p>
-              <p><a href='#About' alt='About Us'>About Us</a></p>
-            </div>
-          </div>
-        )}
       </div>
+      {toggleMenu && (
+        <div className="navbar__menu-container scale-up-center">
+          <div className="navbar__menu-links">
+            <p><a href='#Shop' alt='Shop'>Shop</a></p>
+            <p><a href='#Collections' alt='Collections'>Collections</a></p>
+            <p><a href='#Ourworld' alt='Our world'>Our world</a></p>
+            <p><a href='#About' alt='About Us'>About Us</a></p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
