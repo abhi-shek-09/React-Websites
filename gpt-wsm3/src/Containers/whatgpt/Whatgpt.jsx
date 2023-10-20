6aaa6a54ff4ebdf03aa004a2c323348b33b72eb7
unknown <abhisheklfps@gmail.com>
2023-10-20 16:12:31 +0530

    possibility

--- gpt-wsm3/src/Containers/whatgpt/Whatgpt.jsx
+++ gpt-wsm3/src/Containers/whatgpt/Whatgpt.jsx
@@ -3,7 +3,7 @@ import './Whatgpt.css'
 import Feature from '../../Components/feature/Feature'
 const Whatgpt = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
         <Feature title="What is GPT3?" text= "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
       </div>
