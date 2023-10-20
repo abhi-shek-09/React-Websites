56c2b0813d95b49ca0c6e8838dcafcaf13459584
unknown <abhisheklfps@gmail.com>
2023-10-20 17:23:30 +0530

    blog started

--- gpt-wsm3/src/Containers/blog/Blog.jsx
+++ gpt-wsm3/src/Containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Blog.css'
-
+import Article from '../../Components/article/Article'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+      <div className='gpt3__blog' id='blog'>
+        <div className='gpt3__blog-heading'>
+          <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+        </div>
+        <div className='gpt3__blog-container'>
+          <div className='gpt3__blog-container_groupA'>
+            <Article/>
+          </div>
+
+          <div className='gpt3__blog-container_groupB'>
+            <Article/>
+            <Article/>
+            <Article/>
+            <Article/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
