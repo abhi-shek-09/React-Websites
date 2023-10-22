963b76ec805953a5c242ba97703a71a72e3ce79c
unknown <abhisheklfps@gmail.com>
2023-10-22 15:25:58 +0530

    gpt3 wrapped

--- gpt-wsm3/src/Containers/footer/Footer.jsx
+++ gpt-wsm3/src/Containers/footer/Footer.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './Footer.css'
-
+import gpt3_logo from '../../assets/logo.svg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section_padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request for early access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3_logo} alt='logo'/>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Overons</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132587</p>
+          <p>info@payme.net</p>
+        </div>
+
+        <div className='gpt3__footer-copyright'>
+          <p>© 2021 GPT-3. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
